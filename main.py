cd54e2cc9a603ec734a2a2ea6aa1f47b92f48b1d
alice whaleshark <126924511+wh4l35h4rk@users.noreply.github.com>
2025-04-07 17:43:10 +1000

    linear nonlinear comparison

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.pyplot import legend
 
 a = 0
 b = 10
-N = 400
+N = 150
 h = (b - a) / N
 
 
-def dv(t, v, phi, omega, k, omega_f, A):
+def dv_linear(t, v, phi, omega, k, omega_f, A):
+    return -k * v - omega**2 * phi + A * np.sin(omega_f * t)
+
+def dv_nonlinear(t, v, phi, omega, k, omega_f, A):
     return -k * v - omega**2 * np.sin(phi) + A * np.sin(omega_f * t)
 
 def dphi(v):
     return v
 
 
-def func(t, func_values, omega, k=0, omega_f=0, A=0):
+def func(dv_type, t, func_values, omega, k=0, omega_f=0, A=0):
     v = func_values[0]
     phi = func_values[1]
-    return np.array([dv(t, v, phi, omega, k, omega_f, A),
+    return np.array([dv_type(t, v, phi, omega, k, omega_f, A),
                      dphi(v)])
 
 
-def rungeKutta(x, start_conditions, func, param_list):
+def rungeKutta(x, start_conditions, dv_type, param_list):
     y = np.zeros((1, len(start_conditions)))
     y[0] = start_conditions
 
     for i in range(len(x) - 1):
-        k1 = func(x[i], y[i], *param_list)
-        k2 = func(x[i] + 0.5*h, y[i] + 0.5*h * k1, *param_list)
-        k3 = func(x[i] + 0.5*h, y[i] + 0.5*h * k2, *param_list)
-        k4 = func(x[i] + h, y[i] + h * k3, *param_list)
+        k1 = func(dv_type, x[i], y[i], *param_list)
+        k2 = func(dv_type, x[i] + 0.5 * h, y[i] + 0.5 * h * k1, *param_list)
+        k3 = func(dv_type, x[i] + 0.5 * h, y[i] + 0.5 * h * k2, *param_list)
+        k4 = func(dv_type, x[i] + h, y[i] + h * k3, *param_list)
 
         y_new = y[i] + (h/6) * (k1 + 2 * k2 + 2 * k3 + k4)
         y = np.append(y, [y_new], axis=0)
@@ -39,7 +43,7 @@ def rungeKutta(x, start_conditions, func, param_list):
 
 if __name__ == '__main__':
     v0 = 0
-    phi0 = 0.1
+    phi0 = 1
 
     k = 0
     A = 0
@@ -52,16 +56,22 @@ if __name__ == '__main__':
     t = np.linspace(a, b, N + 1)
 
     parameters = [omega, k, omega_f, A]
-    y = rungeKutta(t, [v0, phi0], func, parameters)
+    y_nonlinear = rungeKutta(t, [v0, phi0], dv_nonlinear, parameters)
+    y_linear = rungeKutta(t, [v0, phi0], dv_linear, parameters)
 
     fig, axs = plt.subplots(1, 2, figsize=(12, 5))
-    axs[0].plot(t, y[:, 1])
+    axs[0].plot(t, y_nonlinear[:, 1], label="Нелинейная модель")
+    axs[0].plot(t, y_linear[:, 1], label="Линейная модель")
     axs[0].set_xlabel(r"$t$, с")
     axs[0].set_ylabel(r"$\phi$, рад")
     axs[0].grid()
-    axs[1].plot(y[:, 1], y[:, 0])
+    axs[0].legend()
+    axs[1].scatter(y_nonlinear[:, 1], y_nonlinear[:, 0], marker='.', label="Нелинейная модель")
+    axs[1].scatter(y_linear[:, 1], y_linear[:, 0], marker='.', label="Линейная модель")
     axs[1].set_xlabel(r"$\phi$, рад")
     axs[1].set_ylabel(r"$v$, рад/с")
     axs[1].grid()
+    axs[1].legend()
+    plt.savefig("linear_nonlinear.png", bbox_inches='tight')
     plt.show()
 
